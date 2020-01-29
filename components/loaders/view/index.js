31d8271b22b8ae24e2147b8bbb89d3270de254e3
Ronan Amsterdam <romanfromrome.com@gmail.com>
2020-01-29 16:59:47 +0800

    hooks and components added

---
+++ components/loaders/view/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Big from './big';
+import Small from './small';
+
+export const VIEW_TYPES = {
+  BIG: "BIG",
+  SMALL: "SMALL"
+}
+
+export default function ({type = VIEW_TYPES.SMALL, loading = true, children, loadedClassName = ''}) {
+
+  const LoaderView = type === VIEW_TYPES.BIG ? Big : Small
+
+  return <>
+    {loading && <LoaderView className={loadedClassName}/>}
+    {!loading && <>{children}</>}
+    </>
+}
---
+++ utils/hooks.js
@@ -0,0 +1,85 @@
+
+import {useEffect, useState, lazy}  from 'react';
+import { useSelector }              from 'react-redux';
+
+import {DEVICE_FORM_FACTORS}    from 'statics/strings/reducers/ux';
+import {updateLocale}           from 'common/utils/locale';
+
+const defaultLocale = {
+    name:       'eng',
+    code:       'en-us',
+    lang:       'en',
+    loaded:     'false',
+    tb:         false,
+  };
+
+function getRelativePath(fullPath = 'src/') {
+  const rootPathReg = new RegExp(/^src\//)
+  const nonRootReg = new RegExp(/(^(.+?)){0,}src\//)
+  if (rootPathReg.test(fullPath)) {
+    // src/my/component
+    return fullPath.replace(rootPathReg, '')
+  } else if (nonRootReg.test(fullPath)) {
+    // if paths is like /something/else/here/src/my/component
+    return fullPath.replace(/(^(.+?)){0,}src\//,'')
+  } else {
+    throw Error(`No matching resource found for path ${fullPath}`)
+  }
+}
+
+export const useLocale = (
+  fullPath = 'src/',
+  config = {}
+) => {
+
+  const relativePath = getRelativePath(fullPath)
+  let {rootKey} = config;
+
+  if (!rootKey) {
+    const rootKeyMatch = relativePath.match(/(?!\/)([A-Za-z-]+)/g);
+
+    if (!rootKeyMatch) {
+      throw Error(`no component locale rootKey found in ${fullPath}`)
+    }
+
+    rootKey = rootKeyMatch.pop();
+  }
+
+  let {locale:selectedLocale} = config;
+  //INFO: no if-else here cuz react hooks cannot be called conditionally
+  const stateLocale = useSelector(store => (store.root && store.root.ux.locale.selected));
+
+  selectedLocale = selectedLocale || stateLocale || defaultLocale;
+
+  const [updatingLocale, setUpdatingLocale] = useState(false);
+  useEffect(() => {
+    setUpdatingLocale(true);
+    updateLocale({
+      rootKey,
+      code:       selectedLocale.code,
+      path:       `${relativePath}/l18n`,
+      cb:         () => setUpdatingLocale(false),
+    })
+  }, [selectedLocale]);
+
+  return {locale: selectedLocale, isLocaleUpdating: updatingLocale};
+}
+
+const getView = (isMobile, relativePath = '') => lazy(() => isMobile ? import(`src/${relativePath}/mobile`) : import(`src/${relativePath}/desktop`))
+
+export const useView = function (fullPath = 'src/') {
+  const relativePath = fullPath.replace(/^src\//,'')
+  const factor = useSelector(state => state.root.ux.device.factor);
+
+  const isMobile = factor === DEVICE_FORM_FACTORS.MOBILE || factor === DEVICE_FORM_FACTORS.TABLET;
+  const [View, setView] = useState(getView(isMobile, relativePath));
+
+  useEffect(()=>{
+    setView(getView(isMobile, relativePath));
+  }, [factor]);
+
+  return {
+    View,
+    isMobile
+  }
+}
