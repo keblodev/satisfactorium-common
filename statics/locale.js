aa0e1ace3f82cd1250c233e1f559d9a229adba8b
Olexandr <alexandr160983@gmail.com>
2022-07-01 16:48:05 +0800

    core brand mods updated

--- statics/locale.js
+++ statics/locale.js
@@ -1,7 +1,9 @@
 export const LOCALE_TYPE_ENG     = 'eng';
 export const LOCALE_TYPE_JP      = '日本語';
+export const LOCALE_TYPE_UKR     = 'ua';
 
 export default [
   LOCALE_TYPE_ENG,
+  LOCALE_TYPE_UKR,
   LOCALE_TYPE_JP,
 ]
